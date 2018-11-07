65f7bf2f2d48896957b85d97b42174e3cf61d2f3
MikeHoangg <hoang.viet.ha.business@gmail.com>
2018-11-07 22:13:40 +0200

    added static and media files urls, did minor refactoring

--- smartfurniture/settings.py
+++ smartfurniture/settings.py
@@ -116,7 +116,7 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL = '/static/'
+MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
