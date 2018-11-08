8ae6bf1e9fa422b8bb64dae5b9c65528e0eb35ec
MikeHoangg <hoang.viet.ha.business@gmail.com>
2018-11-08 13:54:57 +0200

    updated models and  migrations

--- smartfurnitureapi/models.py
+++ smartfurnitureapi/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import AbstractUser
 from django.core.validators import MinValueValidator, RegexValidator, MaxValueValidator
 from django.db import models
 from django.utils import timezone
@@ -7,21 +8,21 @@ from django.utils.translation import gettext_lazy as _
 from smartfurnitureapi import types
 
 
-class User(AbstractBaseUser):
+class User(AbstractUser):
     email = models.EmailField(unique=True)
     image = models.ImageField(null=True,
                               blank=True)
-    height = models.FloatField(validators=[MinValueValidator(0.0)])
-    weight = models.FloatField(validators=[MinValueValidator(0.0)])
-    preferred_rigidity = models.CharField(max_length=32, choices=types.RIGIDITY)
-    USERNAME_FIELD = 'email'
+    height = models.FloatField(validators=[MinValueValidator(0.0)],
+                               null=True,
+                               blank=True)
 
     def __str__(self):
         return self.email
 
 
 class Furniture(models.Model):
-    code = models.CharField(max_length=15,
+    code = models.CharField(unique=True,
+                            max_length=16,
                             validators=[RegexValidator(
                                 regex="^\w+$",
                                 message=_('Code should have only latin symbols and numbers.')
@@ -47,12 +48,22 @@ class Options(models.Model):
     type = models.CharField(max_length=32,
                             choices=types.SOLO_FURNITURE_TYPES + types.MULT_FURNITURE_TYPES)
     name = models.CharField(max_length=32)
-    height = models.FloatField(validators=[MinValueValidator(0.0)])
-    length = models.FloatField(validators=[MinValueValidator(0.0)])
-    width = models.FloatField(validators=[MinValueValidator(0.0)])
-    incline = models.FloatField(validators=[MinValueValidator(0.0), MaxValueValidator(180.0)])
-    rigidity = models.CharField(max_length=32, choices=types.RIGIDITY)
-    creator = models.ForeignKey('User', on_delete=models.CASCADE)
+    height = models.FloatField(validators=[MinValueValidator(0.0)],
+                               null=True,
+                               blank=True)
+    length = models.FloatField(validators=[MinValueValidator(0.0)],
+                               null=True,
+                               blank=True)
+    width = models.FloatField(validators=[MinValueValidator(0.0)],
+                              null=True,
+                              blank=True)
+    incline = models.FloatField(validators=[MinValueValidator(0.0), MaxValueValidator(180.0)],
+                                default=95)
+    rigidity = models.CharField(max_length=32,
+                                choices=types.RIGIDITY,
+                                default='medium')
+    creator = models.ForeignKey('User',
+                                on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
