de4ef530b170fdd598642e9a3a21b432a345eb55
MikeHoangg <hoang.viet.ha.business@gmail.com>
2018-11-29 00:38:28 +0200

    refactored edit-profile template, added functions on closing dialog

--- smartfurnitureclient/src/app/edit-profile/edit-profile.component.ts
+++ smartfurnitureclient/src/app/edit-profile/edit-profile.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ApiService} from "../api.service";
+import {MatDialogRef} from "@angular/material";
 
 @Component({
   selector: 'app-edit-profile',
@@ -6,10 +8,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-profile.component.css']
 })
 export class EditProfileComponent implements OnInit {
+  public error: object;
+  public data: any;
 
-  constructor() { }
+  constructor(private api: ApiService,
+              private dialogRef: MatDialogRef<EditProfileComponent>) {
+    this.data = this.api.currentUser
+  }
 
   ngOnInit() {
   }
-
+  //TODO send image change all dialog to forms
+  save(): void {
+    this.api.editCurrentUser({
+      'username': this.data.username,
+      'email': this.data.email,
+      'first_name': this.data.first_name,
+      'last_name': this.data.last_name,
+      'height': this.data.height,
+    }).subscribe((response: any) => {
+      console.log(response);
+      if (response) {
+        this.dialogRef.close(true);
+        this.error = null;
+        this.api.getCurrentUser().subscribe((response: any) => {
+          if (response)
+            this.api.currentUser = response;
+        });
+      } else
+        this.error = this.api.errorLog.pop();
+    });
+  }
 }
