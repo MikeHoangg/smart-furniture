6e26a97f0b8b00f8059365fe2dfbab4ac14fa2a3
MikeHoangg <hoang.viet.ha.business@gmail.com>
2018-12-02 22:16:19 +0200

    refactored dialogs

--- smartfurnitureclient/src/app/options/options.component.ts
+++ smartfurnitureclient/src/app/options/options.component.ts
@@ -1,6 +1,7 @@
-import {Component, Inject, OnInit} from '@angular/core';
-import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material";
+import {Component, Input, OnInit} from '@angular/core';
+import {MatDialogRef} from "@angular/material";
 import {ApiService} from "../api.service";
+import {FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-options',
@@ -8,18 +9,21 @@ import {ApiService} from "../api.service";
   styleUrls: ['./options.component.css']
 })
 export class OptionsComponent implements OnInit {
-  public error: object;
+  @Input() data: any;
+  title: string;
+  error: any;
+  optionsForm: FormGroup;
 
   constructor(private dialogRef: MatDialogRef<OptionsComponent>,
-              @Inject(MAT_DIALOG_DATA) public data,
               private api: ApiService) {
+    this.title = this.data ? "Add options" : "Edit options";
   }
 
   ngOnInit() {
   }
 
   save(): void {
-    if (this.data.title === "Add") {
+    if (this.title === "Add options") {
       // TODO add options
     } else {
       // TODO edit options
