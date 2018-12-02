381adde778ee99b06288caf0dca9d74890d2adb2
MikeHoangg <hoang.viet.ha.business@gmail.com>
2018-12-02 22:15:29 +0200

    created furniture dialog

--- smartfurnitureclient/src/app/furniture/furniture.component.ts
+++ smartfurnitureclient/src/app/furniture/furniture.component.ts
@@ -1,6 +1,7 @@
-import {Component, Inject, OnInit} from '@angular/core';
-import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material";
+import {Component, Input, OnInit} from '@angular/core';
+import {MatDialogRef} from "@angular/material";
 import {ApiService} from "../api.service";
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-furniture',
@@ -8,18 +9,27 @@ import {ApiService} from "../api.service";
   styleUrls: ['./furniture.component.css']
 })
 export class FurnitureComponent implements OnInit {
-  public error: object;
+  @Input() data: any;
+  title: string;
+  error: any;
+  furnitureForm: FormGroup;
 
   constructor(private dialogRef: MatDialogRef<FurnitureComponent>,
-              @Inject(MAT_DIALOG_DATA) public data,
               private api: ApiService) {
+    this.furnitureForm = new FormGroup({
+      code: new FormControl(this.data ? this.data.code : null),
+      manufacturer: new FormControl(this.data ? this.data.manufacturer : null),
+      type: new FormControl(this.data ? this.data.type : null),
+      is_public: new FormControl(this.data ? this.data.is_public : false),
+    });
+    this.title = this.data ? "Add furniture" : "Edit furniture";
   }
 
   ngOnInit() {
   }
 
   save(): void {
-    if (this.data.title === "Add") {
+    if (this.title === "Add furniture") {
       // TODO add furniture
     } else {
       // TODO edit furniture
