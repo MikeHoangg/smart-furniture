b5da6e302e0fef88a612fc3e48732c9aa530a82a
MikeHoangg <hoang.viet.ha.business@gmail.com>
2019-01-01 13:47:25 +0200

    added localization for backend

--- smartfurnitureapi/api_views.py
+++ smartfurnitureapi/api_views.py
@@ -7,7 +7,6 @@ from django.utils.translation import ugettext_lazy as _
 from django.utils import timezone
 from rest_framework import generics, permissions, status
 from rest_framework.response import Response
-from django.utils.translation import get_language
 from smartfurnitureapi import models, serializers, types
 from smartfurnitureapi.utils import get_iot_data
 
@@ -32,8 +31,7 @@ class FurnitureTypeList(generics.ListAPIView):
 
     @staticmethod
     def get_types_info(types_arr, type_name):
-        return [{'name': i[0], 'type': type_name,
-                 'prime_actions': i[0] in types.PRIME_FURNITURE_TYPES} for i in
+        return [{'name': i[0], 'type': type_name, 'prime_actions': i[0] in types.PRIME_FURNITURE_TYPES} for i in
                 types_arr]
 
     def get_queryset(self):
@@ -73,10 +71,8 @@ class FurnitureList(generics.ListCreateAPIView):
 
     def get_serializer_class(self):
         method = self.request.method
-        if method in ('POST', 'PUT'):
-            return serializers.WriteFurnitureSerializer
-        else:
-            return serializers.ReadFurnitureSerializer
+        return serializers.WriteFurnitureSerializer if method in (
+            'POST', 'PUT') else serializers.ReadFurnitureSerializer
 
 
 class FurnitureDetail(generics.RetrieveUpdateDestroyAPIView):
@@ -85,10 +81,8 @@ class FurnitureDetail(generics.RetrieveUpdateDestroyAPIView):
 
     def get_serializer_class(self):
         method = self.request.method
-        if method in ('POST', 'PUT'):
-            return serializers.WriteFurnitureSerializer
-        else:
-            return serializers.ReadFurnitureSerializer
+        return serializers.WriteFurnitureSerializer if method in (
+            'POST', 'PUT') else serializers.ReadFurnitureSerializer
 
 
 class OptionsList(generics.ListCreateAPIView):
@@ -103,13 +97,13 @@ class OptionsDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = serializers.OptionsSerializer
 
     def perform_update(self, serializer):
-        for f in self.get_object().furniture_set.all():
-            get_iot_data(f)
+        # for f in self.get_object().furniture_set.all():
+        #     get_iot_data(f)
         return super().perform_update(serializer)
 
     def perform_destroy(self, instance):
-        for f in instance.furniture_set.all():
-            get_iot_data(f)
+        # for f in instance.furniture_set.all():
+        #     get_iot_data(f)
         return super().perform_destroy(instance)
 
 
@@ -122,12 +116,8 @@ class LeaveReview(generics.CreateAPIView):
         user = models.User.objects.get(id=request.data.get('user'))
         furniture = models.Furniture.objects.get(id=request.data.get('furniture'))
         if models.Review.objects.filter(user=user, furniture=furniture):
-            if get_language() == 'uk':
-                msg = 'Ви вже залишили рицензію на ці меблі'
-            else:
-                msg = 'You have already reviewed this piece of furniture'
-            return Response({'furniture': msg},
-                            status=status.HTTP_406_NOT_ACCEPTABLE)
+            msg = _('You have already reviewed this piece of furniture')
+            return Response({'furniture': msg}, status=status.HTTP_406_NOT_ACCEPTABLE)
         return super().create(request, *args, **kwargs)
 
 
@@ -147,10 +137,8 @@ class NotificationList(generics.ListCreateAPIView):
 
     def get_serializer_class(self):
         method = self.request.method
-        if method in ('POST', 'PUT'):
-            return serializers.WriteNotificationSerializer
-        else:
-            return serializers.ReadNotificationSerializer
+        return serializers.WriteNotificationSerializer if method in (
+            'POST', 'PUT') else serializers.ReadNotificationSerializer
 
 
 class BrandReviewList(generics.ListAPIView):
@@ -158,10 +146,9 @@ class BrandReviewList(generics.ListAPIView):
     serializer_class = serializers.ReadReviewSerializer
 
     def get(self, request, *args, **kwargs):
-        if not models.Furniture.objects.filter(brand=self.kwargs.get('brand')).exists():
-            return Response({'detail': _('Not found.')}, status=status.HTTP_404_NOT_FOUND)
-        else:
-            return super().get(request, *args, **kwargs)
+        return Response({'detail': _('Not found.')},
+                        status=status.HTTP_404_NOT_FOUND) if not models.Furniture.objects.filter(
+            brand=self.kwargs.get('brand')).exists() else super().get(request, *args, **kwargs)
 
     def get_queryset(self):
         return models.Review.objects.filter(furniture__brand=self.kwargs.get('brand'))
@@ -175,44 +162,30 @@ class ApplyOptions(generics.CreateAPIView):
         furniture = models.Furniture.objects.get(id=request.data.get('furniture'))
         options = models.Options.objects.get(id=request.data.get('options'))
         if furniture.current_users.count() >= 5:
-            if get_language() == 'uk':
-                msg = f'Опції не застосовано до меблів {furniture}, тому що меблями ліміт користувачів досягнено.'
-            else:
-                msg = f'Couldn\'t apply options to furniture {furniture} because user limit has been reached.'
+            msg = _('Couldn\'t apply options to furniture {} because user limit has been reached.').format(furniture)
             stat = status.HTTP_406_NOT_ACCEPTABLE
         elif options.creator in furniture.current_users.all() and options not in furniture.current_options.all():
             furniture.current_options.add(options)
-            for opts in furniture.current_options.all():
-                if options.creator == opts.creator and options.id != opts.id:
-                    furniture.current_options.remove(opts)
+            for o in furniture.current_options.all():
+                if options.creator == o.creator and options.id != o.id:
+                    furniture.current_options.remove(o)
                     break
-            if get_language() == 'uk':
-                msg = f'Опції застосовано до меблів {furniture}'
-            else:
-                msg = f'Options applied to furniture {furniture}.'
+            msg = _('Options applied to furniture {}.').fomat(furniture)
             stat = status.HTTP_202_ACCEPTED
         elif furniture.type in types.SOLO_FURNITURE_TYPES and furniture.current_users.count():
-            if get_language() == 'uk':
-                msg = f'Опції не застосовано до меблів {furniture}, тому що меблями користується {furniture.current_users.first()}.'
-            else:
-                msg = f'Couldn\'t apply options to furniture {furniture} because user {furniture.current_users.first()} is using it.'
+            msg = _('Couldn\'t apply options to furniture {} because user {} is using it.').format(furniture,
+                                                                                                   furniture.current_users.first())
             stat = status.HTTP_406_NOT_ACCEPTABLE
         elif not furniture.is_public and options.creator not in furniture.allowed_users.all() and options.creator != furniture.owner:
-            if get_language() == 'uk':
-                msg = f'Опції не застосовано до меблів {furniture}, тому що у вас немає доступу.'
-            else:
-                msg = f'Couldn\'t apply options to furniture  {furniture} because you have no access to it.'
+            msg = _('Couldn\'t apply options to furniture {} because you have no access to it.').format(furniture)
             stat = status.HTTP_405_METHOD_NOT_ALLOWED
         else:
             furniture.current_users.add(options.creator)
             furniture.current_options.add(options)
-            if get_language() == 'uk':
-                msg = f'Опції застосовано до меблів {furniture}.'
-            else:
-                msg = f'Options applied to furniture {furniture}.'
+            msg = _('Options applied to furniture {}.').format(furniture)
             stat = status.HTTP_202_ACCEPTED
-        if stat == status.HTTP_202_ACCEPTED:
-            get_iot_data(furniture)
+        # if stat == status.HTTP_202_ACCEPTED:
+        #     get_iot_data(furniture)
         return Response({'detail': msg}, status=stat)
 
 
@@ -224,15 +197,12 @@ class DiscardOptions(generics.CreateAPIView):
         furniture = models.Furniture.objects.get(id=request.data.get('furniture'))
         user = models.User.objects.get(id=request.data.get('user'))
         furniture.current_users.remove(user)
-        for options in furniture.current_options.all():
-            if options.creator == user:
-                furniture.current_options.remove(options)
+        for o in furniture.current_options.all():
+            if o.creator == user:
+                furniture.current_options.remove(o)
                 break
-        if get_language() == 'uk':
-            msg = f'Опції зняті з меблів {furniture}.'
-        else:
-            msg = f'Options discarded from furniture {furniture}.'
-        get_iot_data(furniture)
+        msg = _('Options discarded from furniture {}.').format(furniture)
+        # get_iot_data(furniture)
         return Response({'detail': msg}, status=status.HTTP_202_ACCEPTED)
 
 
@@ -249,15 +219,12 @@ class DisallowUser(generics.CreateAPIView):
             notification.save()
         furniture.allowed_users.remove(user)
         furniture.current_users.remove(user)
-        for options in furniture.current_options.all():
-            if options.creator == user:
-                furniture.current_options.remove(options)
-                get_iot_data(furniture)
+        for o in furniture.current_options.all():
+            if o.creator == user:
+                furniture.current_options.remove(o)
+                # get_iot_data(furniture)
                 break
-        if get_language() == 'uk':
-            msg = f'Користувачу {user} не дозволено користуватися меблями {furniture}.'
-        else:
-            msg = f'User {user} is disallowed to use furniture {furniture}.'
+        msg = _('User {} is disallowed to use furniture {}.').format(user, furniture)
         return Response({'detail': msg},
                         status=status.HTTP_202_ACCEPTED)
 
@@ -274,10 +241,7 @@ class AllowUser(generics.CreateAPIView):
             notification.pending = False
             notification.save()
         furniture.allowed_users.add(user)
-        if get_language() == 'uk':
-            msg = f'Користувачу {user} дозволено користуватися меблями {furniture}.'
-        else:
-            msg = f'User {user} is allowed to use furniture {furniture}.'
+        msg = _('User {} is allowed to use furniture {}.').format(user, furniture)
         return Response({'detail': msg},
                         status=status.HTTP_202_ACCEPTED)
 
@@ -311,15 +275,9 @@ class SetPrimeAccount(generics.CreateAPIView):
         expiration_date = now + datetime.timedelta(days=calendar.monthrange(now.year, now.month)[1])
         result = self.create_charge(user, stripe_token, price, expiration_date)
         if result:
-            if get_language() == 'uk':
-                msg = f'Успішне оновлення до prime облікового запису до {expiration_date}.'
-            else:
-                msg = f'Successfully upgraded to prime account till {expiration_date}.'
+            msg = _('Successfully upgraded to prime account till {}.').format(expiration_date)
             stat = status.HTTP_201_CREATED
         else:
-            if get_language() == 'uk':
-                msg = f'Вашу карту відхилено.'
-            else:
-                msg = f'Your card was declined.'
+            msg = _('Your card was declined.')
             stat = status.HTTP_406_NOT_ACCEPTABLE
         return Response({'detail': msg}, status=stat)
